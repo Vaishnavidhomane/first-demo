d7df8ea1d104c08df87f1a3b6d5c42e6a6cffd70
Vaishnavidhomane <vaishnavidhomane29@gmail.com>
2026-01-03 01:12:10 +0530

    Time: 0 ms (100%), Space: 44.7 MB (29.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,45 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int index = -1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) {
-            reverse(nums, 0, n - 1);
-            return;
-        }
-        for (int i = n - 1; i > index; i--) {
-            if (nums[i] > nums[index]) {
-                int temp = nums[i];
-                nums[i] = nums[index];
-                nums[index] = temp;
-                break;
-            }
-        }
-        reverse(nums, index + 1, n - 1);
-    }
+        int n=nums.length;
+        int index=-1;
+        //find if i<i+1
+        //if nasel tr reverse
+        //if asel tr then permutation start honar and then jo next highest and to ghyayecha and then 
+        //swap katayechya bakiche reverse
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+break;
 
-    void reverse(int[] nums, int start, int end) {
+            }
+
+            
+        }
+        if(index==-1){
+            reverse(nums,0,n-1);
+            return;
+
+        }
+        for(int i=n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                int temp=nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
+break;
+
+            }
+            
+        }
+        reverse(nums,index+1,n-1);
+    }
+        void reverse(int[] nums, int start, int end) {
         while (start < end) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
             start++;
             end--;
-        }
-    }
-}
+
+
+
+    }}}
