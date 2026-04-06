dd4569d14004a66da89f814420560b28a89f5fb5
Vaishnavidhomane <vaishnavidhomane29@gmail.com>
2026-04-07 00:42:54 +0530

    Time: 0 ms (100%), Space: 48.4 MB (6.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,35 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        
         long N = n;  
         if (N < 0) {
             x = 1 / x;
             N = -N;
         }
-        
         double result = 1;
         double current = x;
-        
-        
         while (N > 0) {
-            
             if (N % 2 == 1) {
                 result = result * current;
             }
-           
             current = current * current;
             N = N / 2;
         }
-        
         return result;
     }
 }
-
-
-
-
-
-
 // class Solution {
 //     public double myPow(double x, int n) {
 //         double i=1;
@@ -43,22 +30,13 @@ class Solution {
 //             while(n<k){
 //                 b=b*x;
 //                 y=1/b;
-                
 // k--;
-               
-
 //             }
-        
-            
-          
-        
 //         }
 //         if(n>0){
 //         while(i<=n){
 //             y=y*x;
-
 //             i++;
-
 //         }}
 //         return(double) y;
 //     }
